22bf78067c896686522a527a1281aed32a3d405a
ishaan2_4 <ibansal2@illinois.edu>
2022-12-16 13:17:52 -0600

    Ridge detection

--- ray_tracing_curvature.py
+++ ray_tracing_curvature.py
@@ -4,9 +4,11 @@ import numpy as np
 from stl import Mesh
 import matplotlib.pyplot as plt
 import statistics as stats
+import scipy.ndimage
 
 # Write the relative path to the file you want to load
 filename = 'Crater_STL_Files/02_09_2022_250Torr_test3.stl'
+# filename = "Crater_STL_Files/2022_11_01_50mTorr_h10_1s_032gs_noacrylic.stl"
 # Radius for trimming_package; Effects the normal vector and rotation matrix
 trimRadius = 250
 # Radius for trimming the mesh around a point; Effects the histogram and slice
@@ -14,7 +16,7 @@ displayRadius = 250
 # The bounds of the square in which the rays are limited
 bounds = 150
 # The spacing between each ray
-spacing = 10
+spacing = 5
 # If the spacing is great, than the resolution of the data is high and vice versa
 
 normal_vector, lowest_point, rotation_matrix, rotation_matrix_point = help.trimming_package(
@@ -29,6 +31,7 @@ help.trimCircleGivenPoint(new_mesh, lowest_point_r, displayRadius)
 mesh = trimesh.Trimesh(**trimesh.triangles.to_kwargs(new_mesh.vectors))
 mesh.remove_infinite_values()
 mesh.apply_transform(rotation_matrix)
+# mesh.show()
 
 help.move(mesh, lowest_point)
 
@@ -76,6 +79,9 @@ divergence_vis = np.array(divergence_vis)
 gradient = np.gradient(z_locations)
 gradient_norm = np.sqrt(gradient[0]**2 + gradient[1]**2)
 
+double_gradient = np.gradient(gradient_norm)
+double_gradient_norm = np.sqrt(double_gradient[0]**2 + double_gradient[1]**2)
+
 print("X Locations:")
 print(x_locations)
 print("Y Locations:")
@@ -84,26 +90,80 @@ print(y_locations)
 
 fig = plt.figure(1)
 ax = plt.axes()
-CS = ax.contour(x_locations, y_locations, gradient_norm, cmap='turbo')
+CS = ax.contour(x_locations, y_locations, double_gradient_norm, cmap='turbo')
 ax.clabel(CS, inline=True, fontsize=10)
 ax.set_title('2D Gradient Contour')
-plt.show()
 
 fig = plt.figure(2)
 ax = plt.axes(projection='3d')
-ax.contour3D(x_locations, y_locations, gradient_norm, 50, cmap='turbo')
+ax.contour3D(x_locations, y_locations, double_gradient_norm, 50, cmap='turbo')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 ax.set_zlabel('gradient')
 ax.set_title('3D Gradient Contour')
-plt.show()
 
 fig = plt.figure(3)
 img = plt.imshow(z_locations, interpolation='none', cmap='turbo')
-# img.setTitle("Height Heat Image")
-plt.show()
 
 fig = plt.figure(4)
-img = plt.imshow(gradient_norm, interpolation='none', cmap='turbo')
-# img.setTitle("Gradient Heat Image")
+img = plt.imshow(double_gradient_norm, interpolation='none', cmap='turbo')
+# plt.show()
+
+ridge_indices = []
+for theta_deg in range(0, 360, 10):
+    # -- Extract the line...
+    # Make a line with "num" points...
+    theta_rad = np.radians(theta_deg)
+    # These are in _pixel_ coordinates!!
+    radius_x = int(z_locations.shape[0]/2)
+    radius_y = int(z_locations.shape[1]/2)
+    x1, y1 = radius_x - radius_x * \
+        np.cos(theta_rad), radius_y - radius_y*np.sin(theta_rad)
+    x0, y0 = radius_x, radius_y
+    length = int(np.round(np.hypot(x1-x0, y1-y0)))
+    x, y = np.linspace(x0, x1, length), np.linspace(y0, y1, length)
+    mask = (x-radius_x)**2 + (y-radius_y)**2 >= 0.2*(radius_x+radius_y)/2
+    x, y = x[mask], y[mask]
+
+    # Extract the values along the line
+    x = np.floor(x).astype(int)
+    y = np.floor(y).astype(int)
+    z_line = double_gradient_norm[x, y]
+    # x, y = x + z_locations.shape[0], y + z_locations.shape[1]
+
+    # print("X:")
+    # print(x)
+    index = np.argmax(z_line)
+    pair = np.array([x[index], y[index]])
+    ridge_indices.append(pair)
+
+
+ridge_indices = np.array(ridge_indices)
+print(ridge_indices)
+ridge_x = ridge_indices[:, 0]
+ridge_y = ridge_indices[:, 1]
+# plt.figure(5)
+plt.plot(ridge_x, ridge_y, 'ko')
 plt.show()
+
+# radius_x = int(z_locations.shape[0]/2)
+# radius_y = int(z_locations.shape[1]/2)
+# x0, y0 = radius_x - radius_x * \
+#     np.cos(0), radius_y - radius_y*np.sin(0)
+# x1, y1 = radius_x, radius_y
+# length = int(np.round(np.hypot(x1-x0, y1-y0)))
+# x, y = np.linspace(x0, x1, length), np.linspace(y0, y1, length)
+# mask = (x-radius_x)**2 + (y-radius_y)**2 >= 0.2*(radius_x+radius_y)/2
+# x, y = x[mask], y[mask]
+
+# # Extract the values along the line
+# x = x.round().astype(int)
+# y = y.round().astype(int)
+# z_line = double_gradient_norm[x, y]
+# print(z_line.size)
+# print(x.size)
+
+
+# plt.figure()
+# plt.plot(np.sqrt((x-radius_x)**2 + (y-radius_y)**2), z_line)
+# plt.show()
